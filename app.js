f527104346e71c70534353a64735abd6d33e4767
Ken Javier <kencarlo21@gmail.com>
2021-07-18 15:03:36 +0800

    Fixed port

--- app.js
+++ app.js
@@ -68,6 +68,6 @@ app.post("/api/posts", upload.single("post-image") ,(req, res)=>{
     res.status(201).send(newPost);
 })
 
-const port = process.env.port || 5000;
+let port = process.env.PORT || 5000;
 
-app.listen(port, ()=>console.log(`Listening on ${port}`));
+app.listen(port, () => console.log(`Listening on ${port}...`));
